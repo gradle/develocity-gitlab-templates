e5a70073b2e7cfbdfe9aa8c1669571106b3d4c40
Daz DeBoer <daz@gradle.com>
2024-05-14 06:49:54 -0600

    Extract init-script contents out of 'develocity-gradle.yml' (#40)
    * Extract init-scripts out of yml
    
    To make it easier to maintain, the develocity-gradle.yml file is now constructed
    out of a template and 2 init-scripts, which are inlined in the output result.
    
    Added a top-level 'build' script which composes the final output from the constituents.
    
    * Check that generated outputs are up-to-date
    
    * Remove indentation from init-script sources
    
    To make these easier to maintain, indentation is now added by the build process.
    
    * Use 'mkdir -p'
    
    Co-authored-by: Alexis Tual <alexis.tual@gmail.com>
    Signed-off-by: Daz DeBoer <daz@gradle.com>
    
    ---------
    
    Signed-off-by: Daz DeBoer <daz@gradle.com>
    Co-authored-by: Alexis Tual <alexis.tual@gmail.com>

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p build
+
+# Copy the init-scripts to the build directory and indent them for inclusion in the 'develocity-gradle.yml' file.
+sed -e 's/^/      /' src/gradle/init-scripts/build-result-capture.init.gradle > build/build-result-capture.init.gradle
+sed -e 's/^/      /' src/gradle/init-scripts/develocity-injection.init.gradle > build/develocity-injection.init.gradle
+
+# Construct the combined 'develocity-gradle.yml' file from the template and the init-script contents.
+sed -e '/<<BUILD_RESULT_CAPTURE_INIT_GROOVY>>/{
+    r build/build-result-capture.init.gradle
+    d
+}' -e '/<<DEVELOCITY_INJECTION_INIT_GRADLE>>/{
+    r build/develocity-injection.init.gradle
+    d
+}' src/gradle/develocity-gradle.template.yml > develocity-gradle.yml
